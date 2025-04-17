ecd006a8dd93e900289042246952d8dd1dd9fe2b
Niko-Proud <nproud98@gmail.com>
2025-04-16 21:28:56 -0700

    Finished the database as shown in video 3

--- app/src/main/java/com/example/hw04_gymlog_v300/Database/GymLogDAO.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/Database/GymLogDAO.java
@@ -5,18 +5,17 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
-import com.example.hw04_gymlog_v300.Database.GymLogDatabase;
 import com.example.hw04_gymlog_v300.Database.entities.GymLog;
 
-import java.util.List;
+import java.util.ArrayList;
+
 
 @Dao
 public interface GymLogDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(GymLog gymlog);
 
-    @Query("Select * from " + GymLogDatabase.gymlogTable);
-
-    List<GymLog> getAllRecords();
+    @Query("Select * from " + GymLogDatabase.GYM_LOG_TABLE)
+    ArrayList<GymLog> getAllRecords();
 
 }
